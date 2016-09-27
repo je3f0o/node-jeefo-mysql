af1ac5be8efc56b0bb67c83a2942e6dba7afc302
je3f0o <je3f0o@gmail.com>
2016-09-27 18:01:14 +0800

    Added filter method

--- index.js
+++ index.js
@@ -16,14 +16,54 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 //ignore:end
 
 let path             = require("path"),
+	async            = require("async"),
 	config_path      = path.resolve(process.cwd(), "config"),
 	config           = require(config_path).database,
 	mysql_connection = require("./src/mysql_connection");
 
-let mysql_connection_instances = {};
+let mysql_connections_container = {};
+
+let filter = (conn, database, table, data, callback) => {
+	let	column_names = Object.keys(data);
+	
+	async.waterfall(
+		[
+			cb => {
+				let query = `
+					SELECT \`COLUMN_NAME\` FROM \`INFORMATION_SCHEMA\`.\`COLUMNS\`
+						WHERE
+							\`TABLE_SCHEMA\` = ? AND
+							\`TABLE_NAME\`   = ? AND
+							\`COLUMN_NAME\` IN(?)
+				`;
+
+				conn(query, [database, table, column_names], cb);
+			},
+			(columns, f, cb) => {
+				columns = columns.map(c => c.COLUMN_NAME);
+
+				let filtered_data = columns.reduce((payload, key) => {
+					payload[key] = data[key];
+					return payload;
+				}, {});
+				
+				cb(null, filtered_data);
+			}
+		],
+		callback
+	);
+};
 
 module.exports = table => {
-	return mysql_connection_instances[table] || (
-		mysql_connection_instances[table] = mysql_connection(config)
-	);
+	let connection = mysql_connections_container[table];
+
+	if (! connection) {
+		connection = mysql_connections_container[table] = mysql_connection(config);
+		
+		connection.filter = (data, callback) => {
+			filter(connection, config.database, table, data, callback);
+		};
+	}
+
+	return connection;
 };
